50617bb0db948c2029a37dc96acd7653a22b7366
nesibra06-droid <nesibra06@gmail.com>
2026-05-24 22:38:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 
 from groq import Groq
 import streamlit as st
-st.set_page_config(page_title="Generador de rutinas", page_icon="💪")
+from groq import Groq
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-def generar_rutina(objetivo, dias, nivel, feeling):
+def generar_rutina(objetivo, dias, nivel, feeling, nombre):
     respuesta = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
-            {"role": "system", "content":f"Eres un entrenador personal experto en {objetivo}, te caracterizas por saber dar los mejores resultados adaptandote a {feeling} como estado de animo. Usa un tono directo pero seguro y audaz. Empieza SIEMPRE tu respuesta con una frase corta que demuestre confianza, como un entrenador real haría."},
+            {"role": "system", "content":f"Eres un entrenador personal experto en {objetivo}, te caracterizas por saber dar los mejores resultados adaptandote a {feeling} como estado de animo. Usa un tono directo pero seguro y audaz. Empieza SIEMPRE tu respuesta con una frase corta que demuestre confianza, como un entrenador real haría. Siempre dirijete a la persona como {nombre}"},
             {"role": "user", "content": f"Crea una rutina de {objetivo} para {dias} días a la semana, nivel {nivel} y con estado de animo {feeling}"}
 
         ]
@@ -18,6 +18,10 @@ def generar_rutina(objetivo, dias, nivel, feeling):
     st.write(respuesta.choices[0].message.content)
 st.set_page_config(page_title="Generador de rutinas", page_icon="💪")
 st.title("Generador de rutinas con IA")
+nombre = st.text_input("¿Cómo te llamas?")
+
+if nombre:
+    st.write(f"Hola {nombre}, vamos a trabajar.")
 st.write("Tu entrenador personal con IA. Genera una rutina personalizada en segundos.")
 objetivo = st.selectbox("¿Cuál es tu objetivo?", ["fuerza", "cardio", "estudio"])
 dias = st.slider("¿Cuántos días a la semana?", 1, 7)
@@ -27,8 +31,9 @@ if st.button("Generar rutina"):
     if feeling == "Cansado" or feeling == "Sin motivación":
         st.info("Hoy es difícil, pero estás aquí. Eso ya es ganar.")
     with st.spinner("Generando tu rutina..."):
-        generar_rutina(objetivo, dias, nivel, feeling)
+        generar_rutina(objetivo, dias, nivel, feeling, nombre)
         
+
 
 
 
